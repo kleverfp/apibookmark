a62bfdc2bb609fe24af4c5d343bd962c0daeb4d3
Klever Ferreira <kleverfp@gmail.com>
2023-03-03 16:27:41 -0300

    fix: change absolut path to relative

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategy';
 
 @Module({
-  imports: [PrismaModule, JwtModule.register({})],
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
